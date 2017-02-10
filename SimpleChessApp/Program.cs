e3d9f6174507100a4bfb8ec9cbe31700a2bcd1a5
Paul. F <pcfaguiar@gmail.com>
2017-02-10 09:31:42 -0200

    Renamed Game Form

--- SimpleChessApp/Program.cs
+++ SimpleChessApp/Program.cs
@@ -13,7 +13,7 @@ namespace SimpleChessApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmGame());
         }
     }
 }
