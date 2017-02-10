aee7ecbf58694f9d8ba01c65d32b7de855d64cab
Paul. F <pcfaguiar@gmail.com>
2017-02-10 11:39:31 -0200

    Revert "Renamed Game Form"
    This reverts commit e3d9f6174507100a4bfb8ec9cbe31700a2bcd1a5.
    
    Revert form name change to avoid conflict.

--- SimpleChessApp/Program.cs
+++ SimpleChessApp/Program.cs
@@ -13,7 +13,7 @@ namespace SimpleChessApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmGame());
+            Application.Run(new Form1());
         }
     }
 }
