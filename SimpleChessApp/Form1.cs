70604631ed5a777336f6752223579d64904ae9b0
Claudio Moraes <claucometa@gmail.com>
2017-02-10 10:04:05 -0200

    Todo list updated

--- SimpleChessApp/Form1.cs
+++ SimpleChessApp/Form1.cs
@@ -21,16 +21,16 @@ namespace SimpleChessApp
             x.Add(new TodoItem(true, Difficulty.Easy, "4 Pawn movement"));
             // Not implemented
             x.Add(new TodoItem(false, null, ""));
-            x.Add(new TodoItem(false, null, "--------- TO DO LIST ---------"));
+            x.Add(new TodoItem(false, null, "--------- TO DO ---------"));
             x.Add(new TodoItem(false, null, "1 Game Flow"));
             x.Add(new TodoItem(false, Difficulty.Easy, "1.1 Switch players"));
             x.Add(new TodoItem(false, Difficulty.Easy, "1.2 Reset board"));
             x.Add(new TodoItem(false, null, "2 Movement"));
-            x.Add(new TodoItem(false, Difficulty.Hard, "2.1 Movement rules for each piece"));
+            x.Add(new TodoItem(false, Difficulty.Medium, "2.1 Movement rules for each piece"));
             x.Add(new TodoItem(false, Difficulty.Hard, "2.2 Handle move interception"));
             x.Add(new TodoItem(false, null, "2.3 Special moves"));
-            x.Add(new TodoItem(false, Difficulty.Medium, "2.3.1 King castle"));
-            x.Add(new TodoItem(false, Difficulty.Hard, "2.3.2 Check"));
+            x.Add(new TodoItem(false, Difficulty.Easy, "2.3.1 King castle"));
+            x.Add(new TodoItem(false, Difficulty.Medium, "2.3.2 Check"));
             x.Add(new TodoItem(false, null, "3. End Game"));
             x.Add(new TodoItem(false, Difficulty.Hard, "3.1 Stale Mate"));
             x.Add(new TodoItem(false, Difficulty.Hard, "3.2 Check Mate"));
