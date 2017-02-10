7a94f5e7e02c697b8bc7da86abf34f1137bf3ec3
Claudio Moraes <claucometa@gmail.com>
2017-02-10 11:04:27 -0200

    Todo Item Update

--- SimpleChessApp/Form1.cs
+++ SimpleChessApp/Form1.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace SimpleChessApp
 {
@@ -8,39 +7,7 @@ namespace SimpleChessApp
         public Form1()
         {
             InitializeComponent();
-       
-
-            #region TODO LIST
-            var x = new List<TodoItem>();
-            // Implemented
-            x.Add(new TodoItem(false, null, "--------- DONE ---------"));
-            x.Add(new TodoItem(true, Difficulty.Easy, "1 Selection"));
-            x.Add(new TodoItem(true, Difficulty.Easy, "2 Move with Click && Click"));
-            x.Add(new TodoItem(true, Difficulty.Easy, "3 General Capture"));
-            x.Add(new TodoItem(true, Difficulty.Easy, "3.1 Avoid capturing of same color"));
-            x.Add(new TodoItem(true, Difficulty.Easy, "4 Pawn movement"));
-            x.Add(new TodoItem(true, Difficulty.Medium, "5 Pawn promotion"));
-            // Not implemented
-            x.Add(new TodoItem(false, null, ""));
-            x.Add(new TodoItem(false, null, "--------- TO DO ---------"));
-            x.Add(new TodoItem(false, null, "1 Game Flow"));
-            x.Add(new TodoItem(false, Difficulty.Easy, "1.1 Switch players"));
-            x.Add(new TodoItem(false, Difficulty.Easy, "1.2 Reset board"));
-            x.Add(new TodoItem(false, null, "2 Movement"));
-            x.Add(new TodoItem(false, Difficulty.Medium, "2.1 Movement rules for each piece"));
-            x.Add(new TodoItem(false, Difficulty.Hard, "2.2 Handle move interception"));
-            x.Add(new TodoItem(false, null, "2.3 Special moves"));
-            x.Add(new TodoItem(false, Difficulty.Easy, "2.3.1 King castle"));
-            x.Add(new TodoItem(false, Difficulty.Medium, "2.3.2 Check"));
-            x.Add(new TodoItem(false, null, "3. End Game"));
-            x.Add(new TodoItem(false, Difficulty.Hard, "3.1 Stale Mate"));
-            x.Add(new TodoItem(false, Difficulty.Hard, "3.2 Check Mate"));
-            x.Add(new TodoItem(false, Difficulty.Easy, "3.3 Draw by repetition"));            
-            x.Add(new TodoItem(false, Difficulty.Medium, "4 En Passant"));
-            x.Add(new TodoItem(false, Difficulty.Easy, "5 Chess annotation"));
-            #endregion
-
-            dataGridView1.DataSource = x;
+            dataGridView1.DataSource = TodoItem.Items;
             dataGridView1.RowTemplate.Height = 21;
             dataGridView1.Columns[0].Width = 50;
             dataGridView1.Columns[1].Width = 80;
--- SimpleChessApp/TodoItem.cs
+++ SimpleChessApp/TodoItem.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleChessApp
+﻿using System.Collections.Generic;
+
+namespace SimpleChessApp
 {
     class TodoItem
     {
@@ -12,6 +14,42 @@
             Name = n;
             Effort = e;
         }
+
+        public static List<TodoItem> Items
+        {
+            get
+            {
+                var x = new List<TodoItem>();
+                // Implemented
+                x.Add(new TodoItem(false, null, "--------- DONE ---------"));
+                x.Add(new TodoItem(true, Difficulty.Easy, "1 Selection"));
+                x.Add(new TodoItem(true, Difficulty.Easy, "2 General Move with Click & Click"));
+                x.Add(new TodoItem(true, Difficulty.Easy, "2.1 Pawn movement"));
+                x.Add(new TodoItem(true, Difficulty.Easy, "3 General Capture"));
+                x.Add(new TodoItem(true, Difficulty.Easy, "3.1 Avoid capturing of same color"));
+                x.Add(new TodoItem(true, Difficulty.Medium, "4 Pawn promotion"));
+                // Not implemented
+                x.Add(new TodoItem(false, null, ""));
+                x.Add(new TodoItem(false, null, "--------- TO DO ---------"));
+                x.Add(new TodoItem(false, null, "1 Game Flow"));
+                x.Add(new TodoItem(false, Difficulty.Easy, "1.1 Turns"));
+                x.Add(new TodoItem(false, Difficulty.Easy, "1.2 Reset board"));
+                x.Add(new TodoItem(false, null, "2 Movement"));
+                x.Add(new TodoItem(false, Difficulty.Medium, "2.1 Movement rules for each piece"));
+                x.Add(new TodoItem(false, Difficulty.Hard, "2.2 Handle move interception"));
+                x.Add(new TodoItem(false, null, "2.3 Special moves"));
+                x.Add(new TodoItem(false, Difficulty.Easy, "2.3.1 King castle"));
+                x.Add(new TodoItem(false, Difficulty.Medium, "2.3.2 Check"));
+                x.Add(new TodoItem(false, null, "3. End Game"));
+                x.Add(new TodoItem(false, Difficulty.Hard, "3.1 Stale Mate"));
+                x.Add(new TodoItem(false, Difficulty.Hard, "3.2 Check Mate"));
+                x.Add(new TodoItem(false, Difficulty.Easy, "3.3 Draw by repetition"));
+                x.Add(new TodoItem(false, Difficulty.Medium, "4 En Passant"));
+                x.Add(new TodoItem(false, Difficulty.Easy, "5 Chess annotation"));
+
+                return x;
+            }
+        }
     }
 
     enum Difficulty
@@ -20,6 +58,8 @@
         Medium,
         Hard
     }
+
+
 
 }
 
