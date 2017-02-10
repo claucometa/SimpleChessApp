ca4affa219261407bfdd2ab5897ef99bdedfbcd8
Claudio Moraes <claucometa@gmail.com>
2017-02-10 04:12:10 -0200

    TODOList

--- SimpleChessApp/Form1.cs
+++ SimpleChessApp/Form1.cs
@@ -21,18 +21,20 @@ namespace SimpleChessApp
             x.AppendLine("--------------------------------------");
             x.AppendLine("1. Selection");
             x.AppendLine("2. Move with Click && Click");
-            x.AppendLine("3. Capture pieces of opposite colors");
+            x.AppendLine("3. Capture");
+            x.AppendLine("3.1 Avoid capturing of same color");
             x.AppendLine("");
             x.AppendLine("");
             x.AppendLine("TODO LIST");
             x.AppendLine("--------------------------------------");
             x.AppendLine("1. Switch players");
-            x.AppendLine("2. Movement rule for each piece");
+            x.AppendLine("2. Movement rules for each piece");
             x.AppendLine("3. Handle move interception");
             x.AppendLine("4. King castle");
             x.AppendLine("5. Check");
             x.AppendLine("6. Stale Mate");
             x.AppendLine("7. Check Mate");
+            x.AppendLine("8. Pawn promotion");
 
             label1.Text = x.ToString();
         }
