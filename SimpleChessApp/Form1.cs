fb408e1b5caacf6f8603dee59ee08caf6c73e904
Claudio Moraes <claucometa@gmail.com>
2017-02-10 04:17:09 -0200

    Instructions as Resource

--- SimpleChessApp/Form1.cs
+++ SimpleChessApp/Form1.cs
@@ -15,28 +15,7 @@ namespace SimpleChessApp
         public Form1()
         {
             InitializeComponent();
-
-            var x = new StringBuilder();
-            x.AppendLine("Implementations");
-            x.AppendLine("--------------------------------------");
-            x.AppendLine("1. Selection");
-            x.AppendLine("2. Move with Click && Click");
-            x.AppendLine("3. Capture");
-            x.AppendLine("4. Avoid capturing of same color");
-            x.AppendLine("");
-            x.AppendLine("");
-            x.AppendLine("TODO LIST");
-            x.AppendLine("--------------------------------------");
-            x.AppendLine("1. Switch players");
-            x.AppendLine("2. Movement rules for each piece");
-            x.AppendLine("3. Handle move interception");
-            x.AppendLine("4. King castle");
-            x.AppendLine("5. Check");
-            x.AppendLine("6. Stale Mate");
-            x.AppendLine("7. Check Mate");
-            x.AppendLine("8. Pawn promotion");
-
-            label1.Text = x.ToString();
+            label1.Text = Properties.Resources.text;
         }
     }
 }
